c16374613ea250fc0c7822f39acdacb4a7914794
Ryan Sise <kerneltux0@gmail.com>
2020-06-02 11:08:46 -0600

    updated views.py in pages added 2nd key-val pair for testing

--- playingWithDjango/pages/views.py
+++ playingWithDjango/pages/views.py
@@ -4,7 +4,8 @@ from django.http import HttpResponse
 # Create your views here.
 def home_view(request, *args, **kwargs):
     my_context = {
-        'my_text': 'Take a guess'
+        'my_text': 'Take a guess',
+        'this_num': 3.14
     }
     return render(request, "home.html", my_context)
     
