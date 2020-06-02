6ae2b05fbb52593d6fb86f5a2eb5b4221c8c157b
Ryan Sise <kerneltux0@gmail.com>
2020-06-02 09:08:57 -0600

    updated Testing model changed title to CharField type

--- playingWithDjango/testing/models.py
+++ playingWithDjango/testing/models.py
@@ -2,5 +2,5 @@ from django.db import models
 
 # Create your models here.
 class Testing(models.Model):
-    title = models.TextField()
+    title = models.CharField(max_length=120)
     info = models.TextField()
