b13a1dae56650c6793b5e4e3275a4dfdf3670e48
Ryan Sise <kerneltux0@gmail.com>
2020-06-02 11:03:59 -0600

    updated views.py in pages removed comments created my_context dictionary with test data calling my_context inside render

--- playingWithDjango/pages/views.py
+++ playingWithDjango/pages/views.py
@@ -3,9 +3,10 @@ from django.http import HttpResponse
 
 # Create your views here.
 def home_view(request, *args, **kwargs):
-    # return HttpResponse("<h1>Hello World</h1>")
-    return render(request, "home.html", {})
+    my_context = {
+        'my_text': 'Take a guess'
+    }
+    return render(request, "home.html", my_context)
     
 def contact_view(request, *args, **kwargs):
-    # return HttpResponse("<h1>Contact Page</h1>")
     return render(request, "contact.html", {})
