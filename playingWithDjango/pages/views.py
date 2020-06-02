6bddc5c6c83bb5df7126958419d0f77ee59469be
Ryan Sise <kerneltux0@gmail.com>
2020-06-02 09:51:44 -0600

    updated views in pages folder added request as an argument (poss. use later)

--- playingWithDjango/pages/views.py
+++ playingWithDjango/pages/views.py
@@ -2,8 +2,8 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 # Create your views here.
-def home_view(*args, **kwargs):
+def home_view(request, *args, **kwargs):
     return HttpResponse("<h1>Hello World</h1>")
     
-def contact_view(*args, **kwargs):
+def contact_view(request, *args, **kwargs):
     return HttpResponse("<h1>Contact Page</h1>")
