3420d48b5c1c18adbff99fdd5b286e9e259a07f0
Ryan Sise <kerneltux0@gmail.com>
2020-06-03 09:14:00 -0600

    updated views.py in Testing importing Testing setting obj eq to first testing object set context to 3 attr's in the model added 'testing/detail.html' to render

--- playingWithDjango/testing/views.py
+++ playingWithDjango/testing/views.py
@@ -1,5 +1,13 @@
 from django.shortcuts import render
+from .models import Testing
 
 # Create your views here.
 def testing_detail_view(request):
-    return render(request, '', {})
+    obj = Testing.objects.get(id=1)
+    context = {
+        'title': obj.title,
+        'info': obj.info,
+        'coolStuff': obj.coolStuff
+    }
+    
+    return render(request, "testing/detail.html", {})
