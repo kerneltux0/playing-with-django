f34ab0c07b9add172ce248a769bcef67240944fc
Ryan Sise <kerneltux0@gmail.com>
2020-06-03 09:35:53 -0600

    updated views.py in testing commented-out orig. context created new context ref entire object

--- playingWithDjango/testing/views.py
+++ playingWithDjango/testing/views.py
@@ -4,10 +4,14 @@ from .models import Testing
 # Create your views here.
 def testing_detail_view(request):
     obj = Testing.objects.get(id=1)
+
     context = {
-        'title': obj.title,
-        'info': obj.info,
-        'coolStuff': obj.coolStuff
+        'object': obj
     }
+    # context = {
+    #     'title': obj.title,
+    #     'info': obj.info,
+    #     'coolStuff': obj.coolStuff
+    # }
 
     return render(request, "testing/detail.html", context)
