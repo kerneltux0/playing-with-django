44f9f6c600136cba8ec93adafea9b263522d13f0
Ryan Sise <kerneltux0@gmail.com>
2020-06-08 09:13:42 -0600

    updated views.py in testing importing TestForm created new function for form created form variable to use new TestForm passing-in post data

--- playingWithDjango/testing/views.py
+++ playingWithDjango/testing/views.py
@@ -1,7 +1,21 @@
 from django.shortcuts import render
 from .models import Testing
+from .forms import TestForm
 
 # Create your views here.
+
+def testing_create_view(request):
+    form = TestForm(request.POST or None)
+    if form.is_valid():
+        form.save()
+
+    context = {
+        'form': obj
+    }
+
+
+    return render(request, "testing_detail.html", context)
+
 def testing_detail_view(request):
     obj = Testing.objects.get(id=1)
 
