75e64e70be45fb1d151129e508df63d98c076381
Ryan Sise <kerneltux0@gmail.com>
2020-06-08 09:24:45 -0600

    updated urls.py importing testing_create_view set new path 'create,' passing-in testing_create_view

--- playingWithDjango/playingWithDjango/urls.py
+++ playingWithDjango/playingWithDjango/urls.py
@@ -16,11 +16,12 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from pages.views import home_view, contact_view
-from testing.views import testing_detail_view
+from testing.views import testing_detail_view, testing_create_view
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', home_view, name='home'),
     path('contact/', contact_view),
-    path('testing/', testing_detail_view)
+    path('testing/', testing_detail_view),
+    path('create/',testing_create_view)
 ]
