b45dc373901e15c9f203d2c57259685dc6520f96
Ryan Sise <kerneltux0@gmail.com>
2020-06-09 10:54:43 -0600

    updated models.py in testing added get_absolute_url func enables more flexibility in modifying views

--- playingWithDjango/testing/models.py
+++ playingWithDjango/testing/models.py
@@ -1,7 +1,11 @@
 from django.db import models
+from django.urls import reverse
 
 # Create your models here.
 class Testing(models.Model):
     title = models.CharField(max_length=120)
     info = models.TextField()
     coolStuff = models.BooleanField(default=False)
+
+    def get_absolute_url(self):
+        return reverse("testing-detail", kwargs={"id": self.id})
